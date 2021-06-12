558c4a7ca24332ea394cd900cd69a64b8ae4a09d
JJYDXFS <1043804998@qq.com>
2021-06-12 18:30:29 +0800

    为棋盘图添加简介

--- src/components/ChessChart.vue
+++ src/components/ChessChart.vue
@@ -32,7 +32,7 @@ export default defineComponent({
         || document.documentElement.clientHeight
         || document.body.clientHeight;
         // 边距
-        var margin = {top: 10, right: 200, bottom: 60, left: 100},
+        var margin = {top: 10, right: 200, bottom: 60, left: 150},
         width = width*0.87 - margin.left - margin.right,
         height = height*0.9 - margin.top - margin.bottom;
         // 棋盘边长=min(width,height)
@@ -42,34 +42,42 @@ export default defineComponent({
             .append("svg")
                 .attr("width", width + margin.left + margin.right)
                 .attr("height", height + margin.top + margin.bottom);
-        // 交互事件
+        // tooltip
+        var chess_tooltip = d3.select("#my_chess")
+            .append("div")
+            .style("opacity", 0)
+            .attr("class", "chess_tooltip")
+            //.style("background-color", "white")
+            //.style("border", "solid")
+            //.style("border-width", "1px")
+            //.style("border-radius", "5px")
+            //.style("padding", "10px")
+        
         // 交互事件
         var mouseover = function(event,d) {
-            // scatter_tooltip.style("opacity", 1);
-            console.log(d);
+            chess_tooltip.style("opacity",1);
             d3.select(this).attr("stroke-width","4px");
-        }
-    
-        var mousemove = function(event,d) {
+            // 提示框的定位
+            let data=d;
             let point_this = this;
             let point_cx = d3.select(point_this).attr("cx");
-
-            scatter_tooltip
-            .html(d.name+" "+"<br/>"+d.begin_title+"<br/>"+"在任时长："+d.scale+"年")
-            .style("left", function(d){
+            let point_cy = d3.select(point_this).attr("cy");
+            console.log(this);
+            chess_tooltip
+                .html(d.name+" "+"<br/>"+d.info)
+                .style("left", function(d){
+                    if(data.pos_x == 0 || data.name=="黄立极"){
+                        return ( point_cx - 200) + "px"
+                    }
+                    console.log(point_cx+200);
+                    return (point_cx + 30) + "px";
                     
-                if(point_cx < (width-120)){
-                    return (d3.pointer(event,point_this)[0]+30) + "px"
-                }
-                else {
-                    return (d3.pointer(event,point_this)[0]-160) + "px"
-                }
-                    
-            }) // 90是最佳实践
-            .style("top", (d3.pointer(event,this)[1]) + "px")
+                }) // 90是最佳实践
+                .style("top", (point_cy-30) + "px")
         }
             
         var mouseleave = function(event,d) {
+            chess_tooltip.style("opacity",0);
             d3.select(this).attr("stroke-width","2px");
         }
         // 棋盘背景
@@ -79,7 +87,7 @@ export default defineComponent({
         .attr("width",chess_length)
         .attr("xlink:href","img/chess_bg.png");
         // 绘制棋子
-        var img_w=50;
+        var img_w=47;
         var radius=chess_length*0.125*0.35;
         var left_boundry = 202;
         var top_boundry = 40;
@@ -103,7 +111,6 @@ export default defineComponent({
                     return "#0066CC";
                 })
                 .attr("stroke-width","2px")
-                .attr("r",radius)
                 .attr("fill", function(d,i){
                     var defs = svg.append("defs").attr("id","imgdefs");
                     var testpattern = defs.append("pattern")
@@ -118,11 +125,17 @@ export default defineComponent({
                     
                     return "url(#chesspattern"+i+")";
                 });
-
+        // 加交互事件
         chessman.on("mouseover", mouseover)
             .on("mouseleave", mouseleave);
-
-            
+        // 加进入动画
+        chessman.transition()
+            .duration(800)
+            .attr("r",radius*1.04)
+            .transition()
+            .duration(600)
+            .attr("r",radius);
+        
     }
   }
 
@@ -130,5 +143,18 @@ export default defineComponent({
 </script>
 
 <style>
-
+div.chess_tooltip {	
+    position: absolute;			
+    text-align: center;			
+    width: 180px;					
+    height: 60px;					
+    padding: 2px;				
+    font: 14px sans-serif;
+    font-weight: bold;
+    line-height: 20px;	
+    border: 0px;	
+    border-radius: 8px;			
+    pointer-events: none;
+    text-shadow: #fff 0.8px 0 0, #fff 0 0.8px 0, #fff -0.8px 0 0, #fff 0 -0.8px 0;	
+}
 </style>
